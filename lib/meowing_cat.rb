31b2b4f64d1d15095fdcc3eeb108a574a0fc3531
Tugi Ngunjiri <102291617+Tugi-Ngunjiri@users.noreply.github.com>
2023-05-17 20:32:57 +0300

    Initial commit

---
+++ lib/meowing_cat.rb
@@ -0,0 +1,17 @@
+## code your solution here. 
+class Cat
+attr_accessor :name
+end
+Cat = Cat.new
+cat.name = "Cat "
+cat.name
+#=> "Cat "
+end
+def name=(cat_name)
+puts "#{cat_name}"
+end
+
+Class Meow 
+attr_accessor: meow
+puts "meow!"
+end
---
+++ spec/meowing_cat_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'spec_helper'
+require_relative '../lib/meowing_cat'
+
+describe "Cat" do
+  let(:maru) { Cat.new }  # Look up what let does in RSpec
+  # https://www.relishapp.com/rspec/rspec-core/v/2-6/docs/helper-methods/let-and-let
+
+  it 'instantiates a new cat' do
+    expect(maru).to be_a(Cat)
+  end
+
+  it 'receives attr_accessor' do
+    expect(Cat).to receive(:attr_accessor)
+    load('meowing_cat.rb')
+  end
+
+  it 'has a name' do
+    maru.name = "Maru"
+    expect(maru.name).to eq("Maru")
+  end
+
+  it "is able to meow" do
+    expect { maru.meow }.to output(a_string_matching("meow!")).to_stdout
+  end
+end
